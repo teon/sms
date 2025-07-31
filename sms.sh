1c8b5507edc3ca04e40c90a50143f85f54ae717b
Robert Olejnik <robert@olejnik.email>
2025-07-31 21:54:18 +0200

    Added checking args

--- sms.sh
+++ sms.sh
@@ -11,9 +11,21 @@ function usage() {
     echo "send [number] [text]    - send message"
     echo "ls                      - show messages"
     echo "read [number]           - read sms num X"
-    echo "del [number X]          - delete sms X, if not given ALL!"
+    echo "del  [number X]         - delete sms X, if not given ALL!"
     echo "all                     - show ALL smseses"
 }
+
+function check_arg() {
+  local name=$1
+  local arg="$2"
+
+  if [[ -z "$arg" ]]; then
+    echo "ERROR: $name is empty..."
+    usage
+    exit 1
+  fi
+}
+
 
 # get modem number
 MODEMNO=$(mmcli -L | grep -o "Modem/[0-9]" | grep -o [0-9]$)
@@ -21,61 +33,63 @@ MODEMNO=$(mmcli -L | grep -o "Modem/[0-9]" | grep -o [0-9]$)
 case $1 in
 	"send")
 
-	NUMBER=$2
-	TEXT=$3
+	    NUMBER=$2
+	    TEXT=$3
 
-	#create sms in modem and get number
-	SMSNO=$(mmcli -m ${MODEMNO} --messaging-create-sms="text='$TEXT',number=$NUMBER" |  grep -o [0-9]*$)
-	#send message
-	mmcli -s ${SMSNO} --send
-	# delete all sent messages
-	for i in $(mmcli -m ${MODEMNO} --messaging-list-sms | grep " (sent)" | cut -f5 -d' ') ; do
+	    check_arg "number" $NUMBER 
+	    check_arg "text message" $TEXT
+
+	    #create sms in modem and get number
+	    SMSNO=$(mmcli -m ${MODEMNO} --messaging-create-sms="text='$TEXT',number=$NUMBER" |  grep -o [0-9]*$)
+	    #send message
+	    mmcli -s ${SMSNO} --send
+
+	    # delete all sent messages
+	    for i in $(mmcli -m ${MODEMNO} --messaging-list-sms | grep " (sent)" | cut -f5 -d' ') ; do
 		mmcli -m ${MODEMNO} --messaging-delete-sms=$i
-	done
+	    done
 	;;
 
 	"read")
 	
-	SMS=$2
-
-	mmcli -m ${MODEMNO} --sms $SMS
+	    SMS=$2
+	    check_arg "SMS number" $SMS
+	    mmcli -m ${MODEMNO} --sms $SMS
 	
 	;;
 
 	"del")
 
-	SMS=$2
+	    SMS=$2
+	    check_arg "SMS number" $SMS
 
-	if [ "X${SMS}" == "X" ]; then
+	    if [ "X${SMS}" == "X" ]; then
 		echo "Deleging ALL..."
 
 		for i in $(mmcli -m ${MODEMNO} --messaging-list-sms | cut -f5 -d' ') ; do
 			mmcli -m ${MODEMNO} --messaging-delete-sms=$i
 		done
-	else
+	    else
 		mmcli -m ${MODEMNO} --messaging-delete-sms=${SMS}
-	fi
+	    fi
 	;;
 
 	"all")
-	
 		for i in $(mmcli -m ${MODEMNO} --messaging-list-sms | cut -f5 -d' ') ; do
 			mmcli -m ${MODEMNO} --sms $i
 		done
 		;;
 	
 	"ls")
-
 		mmcli -m ${MODEMNO} --messaging-list-sms
 		;;
 
-    "help")
-        usage
-        exit 0
-    ;;
-    *)
-		echo "ERROR: '$1' unknown command"
-        usage
+        "help")
+                usage
+        	exit 0
+    		;;
+    	*)
+        	usage
 		exit 1
 	;;
 esac
